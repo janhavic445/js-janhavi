c4da0c736528b6add129376666fb05d7403655b8
janhavic445 <janhavichoudhary.jc@gmail.com>
2025-05-29 19:15:12 +0000

    datatypes

---
+++ 1_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3 ) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high 
+
+console.log("Janhavi")
+ let name = "Janhavi"
+ let age = 18
+ let isloggedIn = false
+ let state;
+
+ // number => 2 to power of 53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined =>
+ // symbol => unique
+ 
+
+ // object 
+
+ // console.log(typeof "janhavi"); //
+
+ //console.log(typeof age);
+ 
+ console.log(typeof null); //object
+ 
+ console.log(typeof undefined); //undefined
+ 
